573f64127e755af77c11fe95928c83daca3fb6a0
Thomas Laukkanen <tumppu91@hotmail.com>
2021-06-03 10:41:04 +0200

    navworks

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,35 +2,54 @@ import navicon from '../assets/navicon.svg'
 import naviconC from '../assets/naviconC.svg'
 import './Nav.scss'
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 
 function Nav() {
+  const [open, setOpen] = useState(naviconC)
+  useEffect(() => {}, [open])
+
+  let navWrapper = document.querySelector('.navWrapper')
+
   function toggleNav() {
-    document.querySelector('.navWrapper').classList.toggle('showNav')
+    document.querySelector('.navWrapper').classList.toggle('hideNav')
+    navWrapper.classList.contains('hideNav')
+      ? setOpen(navicon)
+      : setOpen(naviconC)
   }
   return (
-    <header className="navWrapper">
-      <nav className="navMain">
-        <button onClick={toggleNav}>
-          <img src={naviconC} alt="nav" />
-        </button>
-        <ul>
-          <li>
-            <Link to="/menu">Menu</Link>
-          </li>
-          <div className="line" />
-          <li>
-            <Link to="/about">Vårt kaffe</Link>
-          </li>
-          <div className="line" />
-          <li>
-            <Link to="/profil">Min profil</Link>
-          </li>
-          <div className="line" />
-          <li>
-            <Link to="/status">Orderstatus</Link>
-          </li>
-        </ul>
-      </nav>
+    <header>
+      <button className="buttonMenu" onClick={toggleNav}>
+        <img src={open} alt="nav" />
+      </button>
+      <div className="navWrapper">
+        <nav className="navMain">
+          <ul>
+            <li>
+              <Link onClick={toggleNav} to="/menu">
+                Menu
+              </Link>
+            </li>
+            <div className="line" />
+            <li>
+              <Link onClick={toggleNav} to="/about">
+                Vårt kaffe
+              </Link>
+            </li>
+            <div className="line" />
+            <li>
+              <Link onClick={toggleNav} to="/profil">
+                Min profil
+              </Link>
+            </li>
+            <div className="line" />
+            <li>
+              <Link onClick={toggleNav} to="/status">
+                Orderstatus
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   )
 }
