4f73e07308d16b2847d15c0c7a1e4e0c44d690eb
Thomas Laukkanen <tumppu91@hotmail.com>
2021-06-04 15:05:54 +0200

    status finnished

--- src/views/Status.js
+++ src/views/Status.js
@@ -1,5 +1,19 @@
+import './Status.scss'
+import drone from '../assets/drone.svg'
 function Status() {
-  return <div></div>
+  return (
+    <div className="statusWrapper">
+      <h3>
+        Ordernummer <strong>#12DV23F </strong>
+      </h3>
+      <img src={drone} alt="drone" />
+      <h1>Din beställning är på väg!</h1>
+      <h2>
+        <strong>13</strong> minuter
+      </h2>
+      <button>Ok, cool!</button>
+    </div>
+  )
 }
 
 export default Status
